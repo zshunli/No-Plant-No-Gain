29971380769c7e1d43a7502ee7123fe73e2ae704
zshunli <shunli1313@gmail.com>
2021-12-22 16:51:36 +0800

    index大致完成

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,10 +2,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("bar").style.padding = "0 0";
-    document.getElementById("logo").style.fontSize = "20px";
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
   } else {
-    document.getElementById("bar").style.padding = "50px 0";
+    document.getElementById("navbar").style.padding = "80px 10px";
     document.getElementById("logo").style.fontSize = "35px";
   }
 }
