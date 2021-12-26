d68d73c8d348eba09bed2bca3d4f83105ec7673a
zshunli <shunli1313@gmail.com>
2021-12-27 00:32:34 +0800

    購物車

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,48 +1,42 @@
 var app = new Vue({
-    el:'#app',
-    data:{
-        itemList:[
-          {
-            id:'1',
-            itemName:'優質短袖白T',
-            price:'500',
-            count:'2'
-          },
-          {
-            id:'2',
-            itemName:'骷髏手短黑T',
-            price:'790',
-            count:'3'
-          },
-          {
-            id:'3',
-            itemName:'超時尚牛仔庫',
-            price:'1200',
-            count:'1'
-          },
-          {
-            id:'4',
-            itemName:'質感褐色系大衣服',
-            price:'2300',
-            count:'1'
-          },
-    ]
-    },
-    methods:{
-        handlePlus: function(item){
-            item.count++;
+  el:'#app',
+  data:{
+      itemList:[
+        {
+          id:'1',
+          itemName:'金柳',
+          price:'345',
+          count:'2'
         },
-        handleSub: function(item){
-            if(item.count>1){
-            item.count--;                
-            }
+        {
+          id:'2',
+          itemName:'白葉到手香',
+          price:'130',
+          count:'1'
         },
-        handledelete: function(index){
-            console.log(this);
-            this.itemList.splice(index,1);
-        }
-    },
-    computed:{
+        {
+          id:'3',
+          itemName:'日本三河黑松-(4寸)',
+          price:'450',
+          count:'1'
+        },
+  ]
+  },
+  methods:{
+      handlePlus: function(item){
+          item.count++;
+      },
+      handleSub: function(item){
+          if(item.count>1){
+          item.count--;                
+          }
+      },
+      handledelete: function(index){
+          console.log(this);
+          this.itemList.splice(index,1);
+      }
+  },
+  computed:{
 
-    }
+  }
 })
